2999bb17211b28107914625992e744947a4ded92
John Folberth <johnfolberth@gmail.com>
2023-03-23 22:46:20 -0400

    add oauth.ts

---
+++ code/src/oauth.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export const OAuthSettings = {
+  appId: '932e593a-6863-468a-8ed8-62cbeddc2ef1',
+  redirectUri: 'http://localhost:4200',
+  scopes: ['user.read', 'mailboxsettings.read', 'calendars.readwrite'],
+};
